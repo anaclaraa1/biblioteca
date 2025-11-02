fffc5619d829b878c0d1017113f2b05ef8acdb62
Cecilya <anacecilya17@gmail.com>
2025-11-01 21:40:48 -0300

    Adicionar a rota de register, login e logout do usuário

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from sqlalchemy.orm import Session
+from flask_login import LoginManager
+from database.models import Usuarios, Base
+from controllers.auth import auth_bp
+from controllers.livro import livro
+from database import engine
+
+app = Flask(__name__)
+app.secret_key = 'Esconda-me'
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    with Session(bind=engine) as db:
+        user = db.query(Usuarios).where(Usuarios.id == user_id).first()
+    return user
+# Base.metadata.create_all(bind=engine)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# app.register_blueprint(products.bp)
+app.register_blueprint(auth_bp)
+app.register_blueprint(livro)
---
+++ controllers/auth.py
@@ -0,0 +1,45 @@
+from flask import render_template, url_for, request,redirect, Blueprint
+from sqlalchemy.orm import Session
+from werkzeug.security import generate_password_hash, check_password_hash
+from database import engine
+from flask_login import login_user, login_required, logout_user
+from database.models import Usuarios
+from datetime import date
+auth_bp = Blueprint('auth_bp', __name__, template_folder='../templates')
+# bp = Blueprint('users', __name__)
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        nome = request.form.get('nome')
+        email = request.form.get('email')
+        telefone = request.form.get('tel')
+        data_atual = date.today()
+        with Session(bind=engine) as db:
+            user_exist = db.query(Usuarios).where(Usuarios.Email == email).first()
+            if not user_exist:
+                user_novo = Usuarios(Nome_usuario=nome, Email=email, Data_inscricao=data_atual, Numero_telefone=telefone, Multa_atual=0)
+                db.add(user_novo)
+                db.commit()
+                login_user(user_novo)
+                return redirect(url_for('index'))
+            #f
+    return render_template('user/register.html')
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        with Session(bind=engine) as db:
+            user_exist = db.query(Usuarios).where(Usuarios.Email == email).first()
+        if user_exist:
+            login_user(user_exist)
+            return redirect(url_for('index'))
+        #f
+    return render_template('user/login.html')
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
