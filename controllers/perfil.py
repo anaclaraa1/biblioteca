b772d009e5ca2f3885a2ca234f0c57c9df1f5267
Maria <garciamariadasgracas318@gmail.com>
2025-11-05 14:19:30 -0300

    criar rotas para editar e excluir perfil

--- controllers/perfil.py
+++ controllers/perfil.py
@@ -1,7 +1,8 @@
-from flask import render_template, Blueprint
-from flask_login import login_required, current_user
+from flask import render_template, request, Blueprint, flash, redirect, url_for
+from flask_login import login_required, current_user, logout_user
 from database import engine
 from sqlalchemy import text
+from sqlalchemy.exc import IntegrityError
 
 perfil = Blueprint('perfil', __name__, template_folder='../templates')
 
@@ -15,3 +16,46 @@ def visualizar():
         user = data.fetchone()
         
     return render_template('user/perfil.html', user=user)
+
+@perfil.route('/editar_perfil/<int:user_id>', methods=['GET', 'POST'])
+@login_required
+def editar_perfil(user_id: int):
+    if request.method == 'POST':
+        with engine.begin() as conn:
+            conn.execute(
+                text(
+                    '''
+                        UPDATE usuarios
+                        SET Nome_usuario = :nome,
+                        Email = :email,
+                        Numero_telefone =:tel
+                        WHERE ID_usuario = :user_id
+                    '''
+                ),
+                {**request.form, 'user_id': user_id}
+            )
+            conn.commit()
+        flash('Dados alterados com sucesso', category='success')
+        return redirect(url_for('perfil.visualizar'))
+    
+    with engine.begin() as conn:
+        data = conn.execute(text('SELECT * FROM usuarios WHERE ID_usuario =: user_id'), {'user_id': user_id})
+        user = data.fetchone()
+    return render_template('user/editar.html', user=user)
+
+@perfil.route('/deletar_perfil/<int:user_id>', methods=['GET'])
+@login_required
+def deletar_perfil(user_id: int):
+    with engine.begin() as conn:
+        conn.execute(
+            text('DELETE FROM emprestimos WHERE Usuario_id = :user_id'),
+            {'user_id': user_id}
+        )
+        conn.execute(
+            text('DELETE FROM usuarios WHERE ID_usuario = :user_id'),
+            {'user_id': user_id}
+        )
+        conn.commit()
+    logout_user()
+    flash('Perfil deletado com sucesso!', category='success')
+    return redirect(url_for('index'))
